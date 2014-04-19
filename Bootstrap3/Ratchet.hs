405323c7261cbcde3dfc6edad07bcd9c9a496b0b
Lucas <lucasnunno@gmail.com>
2014-04-19 15:25:29 -0600

    Added Ratchet file.

---
+++ Bootstrap3/Ratchet.hs
@@ -0,0 +1,12 @@
+module Ratchet where
+	{-
+	A mobile-first front-end HTML5 library.
+	http://goratchet.com/
+	-}
+	import Text.Blaze.Html5 hiding (head,map)
+    import Text.Blaze.Html5.Attributes
+    import Text.Blaze.Html5 as H hiding (head,map)
+    import Text.Blaze.Html5.Attributes as A
+    import Text.Blaze.Html.Renderer.Pretty
+    import Models
+    import Utils
